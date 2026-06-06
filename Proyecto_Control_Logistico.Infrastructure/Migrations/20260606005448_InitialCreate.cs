ac543bb127af7fb972bf8ecdb3c90cea848bfbc3
Vladimir <cdtvladmir0992@gmail.com>
2026-06-05 22:16:48 -0500

    fix: Aplicación de bootstrap para el partial view de crear y volver

---
+++ Proyecto_Control_Logistico.Infrastructure/Migrations/20260606005448_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_Control_Logistico.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NumberOrder",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_NumberOrder",
+                table: "Orders",
+                column: "NumberOrder",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_NumberOrder",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NumberOrder",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
