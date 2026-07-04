40ff0a6fc17eb547fc5dd19142f163ccbed6f49b
Luis Cordova <j.sistemas@coopactocache.com.pe>
2026-07-03 19:12:18 -0500

    crear rama en git

---
+++ Proyecto_Control_Logistico/Areas/Admin/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Control_Logistico.Infrastructure.Repositories.IRepository;
+using Proyecto_Control_Logistico.UI.MVC.Utils;
+
+namespace Proyecto_Control_Logistico.UI.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ClientController : BaseController
+    {
+        public ClientController(IUnitOfWork unitOfWork, ILogger<BaseController> logger, IMapper mapper) : base(unitOfWork, logger, mapper)
+        {
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
