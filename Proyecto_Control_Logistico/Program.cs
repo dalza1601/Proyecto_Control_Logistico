78afd55146e41effb4c70094635aadf8dc1338ab
Diego <dalza.1601@gmail.com>
2026-07-08 19:42:53 -0500

    Se agrega una nueva migracion y merge con la rama de vladimir.

--- Proyecto_Control_Logistico/Program.cs
+++ Proyecto_Control_Logistico/Program.cs
@@ -18,7 +18,7 @@ using Proyecto_Control_Logistico.Infrastructure.Seed;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("SQLSERVER_CONECTION") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("SQLSERVER_AZURE_Connection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
