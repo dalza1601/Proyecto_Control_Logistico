aa9d71607140adf8a8521e475b934d266fab8337
jolubk <jolubk@hotmail.com>
2026-07-10 17:20:21 -0500

    corrección de dto warehouse
    se corrige el nombre de a clase por WareHouse

--- Proyecto_Control_Logistico.Application/DTOs/WarehouseDTO.cs
+++ Proyecto_Control_Logistico.Application/DTOs/WarehouseDTO.cs
@@ -1,8 +1,21 @@
-﻿namespace Proyecto_Control_Logistico.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Control_Logistico.Application.DTOs
 {
-    public class WarehouseDTO
+    public class WareHouseDTO
     {
-        public string Name { get; set; }
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Dirección")]
+        [Required(ErrorMessage = "La dirección es requerida")]
+        [MaxLength(250, ErrorMessage = "La dirección no puede exceder los 250 caracteres")]
+        public string Address { get; set; } = string.Empty;
+
         public int Inputs { get; set; }
         public int Outputs { get; set; }
     }
