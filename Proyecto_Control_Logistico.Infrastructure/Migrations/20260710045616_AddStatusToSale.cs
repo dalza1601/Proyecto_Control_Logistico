275e7fdda1820e8bb934449000df66fe3ce9ce20
MOISES ALAMA <wilson_1665@hotmail.com>
2026-07-09 23:57:32 -0500

    feat: crear migracion para estado de ventas

---
+++ Proyecto_Control_Logistico.Infrastructure/Migrations/20260710045616_AddStatusToSale.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_Control_Logistico.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToSale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Sales",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Sales");
+        }
+    }
+}
