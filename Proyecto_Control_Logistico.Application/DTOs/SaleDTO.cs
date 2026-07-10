6a761fece1db847fdd50967936f6526cce8dddd4
MOISES ALAMA <wilson_1665@hotmail.com>
2026-07-09 23:33:17 -0500

    feat: crear DTOs para ventas

---
+++ Proyecto_Control_Logistico.Application/DTOs/SaleDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Control_Logistico.Application.DTOs
+
+{
+    public class SaleDTO
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Número de venta")]
+        public string NumberSale { get; set; } = string.Empty;
+
+        [Display(Name = "Cliente")]
+        public int ClientId { get; set; }
+
+        [Display(Name = "Cliente")]
+        public string ClientName { get; set; } = string.Empty;
+
+        [Display(Name = "Fecha de venta")]
+        public DateTime DateSale { get; set; }
+
+        [Display(Name = "Total")]
+        public decimal TotalAmount { get; set; }
+
+        [Display(Name = "Estado")]
+        public string Status { get; set; } = "Registrado";
+
+        public List<SaleDetailDTO> Details { get; set; } = new();
+
+    }
+}
