6a413ffe2eb26ea9763ca08fd0d26b38d31520f4
MOISES ALAMA <wilson_1665@hotmail.com>
2026-07-10 00:02:22 -0500

    feat: agregar mapeo para ventas

--- Proyecto_Control_Logistico.Application/Mapping/MappingHelper.cs
+++ Proyecto_Control_Logistico.Application/Mapping/MappingHelper.cs
@@ -10,6 +10,15 @@ namespace Proyecto_Control_Logistico.Application.Mapping
         
             CreateMap<Category, CategoryDTO>().ReverseMap();
             CreateMap<Client,ClientDTO>().ReverseMap();
+
+            CreateMap<SaleDetail, SaleDetailDTO>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product != null ? src.Product.Name : string.Empty))
+                .ReverseMap();
+
+            CreateMap<Sale, SaleDTO>()
+                .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src => src.Client != null ? src.Client.FullName : string.Empty))
+                .ForMember(dest => dest.Details, opt => opt.MapFrom(src => src.SaleDetails))
+                .ReverseMap();
         }
     }
 }
